48370b861f9b9f7c05608145722a8fb89641f2cd
Nigel Baillie <metreckk@gmail.com>
2015-07-13 13:00:23 -0400

    (12) beginning devise with json responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_devise_paramters, if: :devise_controller?
+
+  protected
+
+  def configure_devise_paramters
+    devise_parameter_sanitizer.for(:sign_up) << :first_name
+    devise_parameter_sanitizer.for(:sign_up) << :last_name
+  end
 end
---
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe Users::RegistrationsController do
+  let(:result) { JSON.parse(response.body).with_indifferent_access }
+
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
+  describe 'POST #create' do
+    context 'given an email, password, and password_confirmation (> 8 characters)' do
+      it 'creates a user and returns its json representation' do
+        post :create, format: :json, user: {
+          email: 'test@example.com', password: 'pw4tester22', password_confirmation: 'pw4tester22'
+        }
+        expect(response).to be_ok
+
+        expect(result[:email]).to eq 'test@example.com'
+        expect(User.where(email: 'test@example.com')).to exist
+      end
+    end
+
+    context 'given an email, password, and mismatched password_confirmation' do
+      it 'does not create a user and returns the errors as json' do
+        post :create, format: :json, user: {
+          email: 'test@example.com', password: 'pw4tester22', password_confirmation: 'woopsmyhandsslipped'
+        }
+        expect(response.status).to eq 422
+
+        expect(result[:errors]).to include "Password confirmation doesn't match Password"
+      end
+    end
+  end
+end
